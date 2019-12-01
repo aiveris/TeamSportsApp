81f4d44734d1fdb6df631d0c0cf0319e263ce4f6
Ignas <kapitonsIgnas@gmail.com>
2019-12-01 14:06:31 +0200

    Hibernate util and properties

--- src/main/java/com/montini/teamsports/HibernateUtil.java
+++ src/main/java/com/montini/teamsports/HibernateUtil.java
@@ -2,6 +2,9 @@ package com.montini.teamsports;
 
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
@@ -10,22 +13,37 @@ import org.hibernate.service.ServiceRegistry;
 import java.util.Properties;
 
 public class HibernateUtil {
+    private static StandardServiceRegistry registry;
     private static SessionFactory sessionFactory;
+
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration();
+                // Create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
 
+                // Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
 
+                // Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
 
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             } catch (Exception e) {
                 e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
             }
         }
         return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
     }
 }
