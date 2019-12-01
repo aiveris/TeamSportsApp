7633514564dc13b4d7ec320ecc6ccd51158b9ee8
Ignas <kapitonsIgnas@gmail.com>
2019-12-01 15:25:52 +0200

    Hibernate util and properties

--- src/test/java/com/montini/teamsports/LocationTests.java
+++ src/test/java/com/montini/teamsports/LocationTests.java
@@ -1,17 +1,52 @@
 package com.montini.teamsports;
 
+import com.montini.teamsports.dao.LocationDao;
 import com.montini.teamsports.model.Location;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 public class LocationTests {
     private static final Logger log = LoggerFactory.getLogger( Location.class );
 
+    private SessionFactory sf;
+
+
+
+
+    @Test
+    public void test() {
+        Session session = null;
+        Transaction txn = null;
+        try {
+            session = sf.openSession();
+            txn = session.beginTransaction();
+
+            Location location = new Location();
+            location.setName("Impulsas");
+            session.persist(location);
+
+            txn.commit();
+        } catch (RuntimeException e) {
+            if ( txn != null && txn.isActive() ) txn.rollback();
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 
 }
