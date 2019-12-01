8f7d36c231b37288e6cc9ddf54ba99f7bdcbd7bd
Ignas <kapitonsIgnas@gmail.com>
2019-12-01 11:59:09 +0200

    Merge branch 'master' of D:\PROJECT with conflicts.

--- src/main/java/com/montini/teamsports/model/Player.java
+++ src/main/java/com/montini/teamsports/model/Player.java
@@ -1,13 +1,19 @@
 package com.montini.teamsports.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
 import java.util.List;
 
-// @Entity
-public class Player {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+public class Player implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     private String username, password, email;
     private int rank;
@@ -23,6 +29,44 @@ public class Player {
 
     // Getters and Setters
 
-    // Methods
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
 }
