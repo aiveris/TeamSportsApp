8c1f45a9b1ad77bba8da5e9ab7482ed65da89496
Ignas <kapitonsIgnas@gmail.com>
2019-12-01 14:59:43 +0200

    Hibernate util and properties

---
+++ src/main/java/com/montini/teamsports/dao/LocationDaoImpl.java
@@ -0,0 +1,135 @@
+package com.montini.teamsports.dao;
+
+import com.montini.teamsports.HibernateUtil;
+import com.montini.teamsports.model.Location;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class LocationDaoImpl implements  LocationDao{
+
+    @Autowired
+    Location location;
+
+
+    public void saveLocation(Location location) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(location);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Update Location
+     * @param location
+     */
+    public void updateLocation(Location location) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.update(location);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Delete Instructor
+     * @param id
+     */
+    public void deleteInstructor(int id) {
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // Delete a location object
+            Location location = session.get(Location.class, id);
+            if (location != null) {
+                session.delete(location);
+                System.out.println("instructor is deleted");
+            }
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get Location By ID
+     * @param id
+     * @return
+     */
+    public Location getLocation(int id) {
+
+        Transaction transaction = null;
+        Location location = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get an instructor object
+            location = session.get(Location.class, id);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    /**
+     * Get all Location
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public List<Location> getAllLocation() {
+
+        Transaction transaction = null;
+        List<Location> listOfLocation = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get an instructor object
+
+            listOfLocation = session.createQuery("from Location").getResultList();
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return listOfLocation;
+    }
+}
