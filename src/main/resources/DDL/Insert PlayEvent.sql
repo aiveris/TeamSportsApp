819138e8d58229a3063d996f976927de7d570d0d
montini <energotronic1>
2019-12-09 15:47:34 +0200

    Updates

--- src/main/resources/DDL/Insert PlayEvent.sql
+++ src/main/resources/DDL/Insert PlayEvent.sql
@@ -1,2 +1,2 @@
-INSERT INTO playevent VALUES (NULL, 1, "2019-12-01", "Badminton", "SO", 2, "Jonas", "Jonas, Petras", 2)
+INSERT INTO playevent VALUES (NULL, 1, '2019-12-01', 'Badminton', 'SO', 2, 'Jonas', 'Jonas, Petras', 2)
 
