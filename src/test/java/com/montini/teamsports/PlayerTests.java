c00a74f5e1a4e26626bf96c56a19f5e7b8560cdc
Ignas <kapitonsIgnas@gmail.com>
2019-12-15 12:39:54 +0200

    Crud Problem

--- src/test/java/com/montini/teamsports/PlayerTests.java
+++ src/test/java/com/montini/teamsports/PlayerTests.java
@@ -2,36 +2,19 @@ package com.montini.teamsports;
 
 import com.montini.teamsports.dao.UserRepository;
 import com.montini.teamsports.dao.UserRepositoryImpl;
-import com.montini.teamsports.model.Authority;
-import com.montini.teamsports.model.AuthorityType;
 import com.montini.teamsports.model.Player;
 import com.montini.teamsports.model.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.HashSet;
-import java.util.Set;
-
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
-@WebAppConfiguration("classpath:resources")
-@ComponentScan("com.montini.teamsports")
 public class PlayerTests {
     public static final Logger log = LoggerFactory.getLogger(Player.class);
 
-    @Autowired
     UserRepository userRepository = new UserRepositoryImpl();
-    @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
@@ -43,12 +26,7 @@ public class PlayerTests {
             txn = session.beginTransaction();
 
             User user = new User();
-            Set<Authority> authorities = new HashSet<>();
-            Authority authority = new Authority(AuthorityType.valueOf("ROLE_ADMIN"));
-            authorities.add(authority);
-            user.setId(5);
             user.setUsername("Ignas");
-            user.setAuthorities(authorities);
             user.setPassword(bCryptPasswordEncoder.encode("asdfg"));
             userRepository.add(user);
 
